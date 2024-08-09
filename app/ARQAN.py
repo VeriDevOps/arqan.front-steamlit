d7747fcf8baff5b04019ddf4db35c7ae2e0f9190
Andrey Sadovykh <sadovykh@gmail.com>
2024-08-09 17:00:30 +0200

    tunning

--- app/ARQAN.py
+++ app/ARQAN.py
@@ -20,23 +20,27 @@ security_image = Image.open("./utils/secreq.png")
 with col1:
     st.image(stig_image, width=101)  # Adjust the width as needed
     st.header("STIG Search")
-    st.write("""
+    st.write(
+        """
     For a given textual security requirement and a specific platform (e.g., Windows 10),
     this tool searches relevant Security Technology Implementation Guidelines (STIG).
     It provides a recipe for detailed security requirements or even a fix.
-    """)
+    """
+    )
     st.page_link("pages/1_STIG_Search.py", label="STIG Search", icon="🔍")
 
 # Prototype 2: Security Requirements Extraction
 with col2:
     st.image(security_image, width=150)  # Adjust the width as needed
     st.header("SecReq Extraction")
-    st.write("""
+    st.write(
+        """
     Upload a PDF file, and this tool will analyze it to extract security-related requirements.
     It is designed to identify and present specific security needs found within the document.
-    """)
+    """
+    )
     st.page_link("pages/2_SecReq_Extraction.py", label="SecReq Extraction", icon="📤")
 
 # Optionally, add a footer or additional information
 st.markdown("---")
-st.write("Select one of the tools above to start using them.")
+st.write("Login. Select one of the tools above to start experimenting.")
